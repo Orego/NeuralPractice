4ae39bd3fcf2b7c01674ec2afec5ce13b85ccb43
Peter Drake <drake@lclark.edu>
2015-05-22 14:26:33 -0700

    Added @Override tags to InputNeuron.

--- src/InputNeuron.java
+++ src/InputNeuron.java
@@ -3,10 +3,12 @@ public class InputNeuron implements Neuron {
 
 	private double activation; 
 	
+	@Override
 	public double getActivation() {
 		return activation;
 	}
 
+	@Override
 	public void setActivation(double act) {
 		activation = act;
 	}
