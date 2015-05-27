326ac024a383bc228c205f4f8343573829849245
gdaaronson <gdaaronson@gmail.com>
2015-05-27 10:09:29 -0700

    Fixed errors from adding constructor

--- src/Network.java
+++ src/Network.java
@@ -4,51 +4,52 @@ public class Network {
 	InputNeuron bias = new InputNeuron();
 	InputNeuron a = new InputNeuron();
 	InputNeuron b = new InputNeuron();
-	SigmoidNeuron output = new SigmoidNeuron();
+	SigmoidNeuron output;
 	double learningRate = 1;
 	double weightMin = -1;
 	double weightMax = 1;
 
-
-	public Network(){
-		bias.setActivation(1);								//Set bias activation to 1
-		Neuron[] inputs= {a, b, bias};
-		output.setInputs(inputs);
-		double[] weightArray = new double [output.getInputSize()];
-		for(int i = 0; i < 3; i++){
-			weightArray[i] = rand();						//Initialize Weights
+	public Network() {
+		bias.setActivation(1); // Set bias activation to 1
+		Neuron[] inputs = { a, b, bias };
+		double[] weightArray = new double[output.getInputSize()];
+		for (int i = 0; i < inputs.length; i++) {
+			weightArray[i] = rand(); // Initialize Weights
 		}
-		output.setWeights(weightArray);
+		output = new SigmoidNeuron(inputs, weightArray);
 	}
 
-	/**Returns a random double within specified range*/
-	public double rand(){
+	/** Returns a random double within specified range */
+	public double rand() {
 		Random rand = new Random();
-		return  weightMin + (weightMax - weightMin) * rand.nextDouble();
+		return weightMin + (weightMax - weightMin) * rand.nextDouble();
 	}
-	
-	/**Calculates and returns the delta value (double) for output neuron*/
-	public double calculateDeltaOutput(double correct){
-//		System.out.println(correct + " " + (output.getActivation() * (1 - output.getActivation()) * (correct - output.getActivation())));
-		return (output.getActivation() * (1 - output.getActivation()) * (correct - output.getActivation()));
-	}
-	
-	public double calculateDeltaHidden(double correct){
 
-		return (output.getActivation() * (1 - output.getActivation()) * (correct - output.getActivation()));
+	/** Calculates and returns the delta value (double) for output neuron */
+	public double calculateDeltaOutput(double correct) {
+		// System.out.println(correct + " " + (output.getActivation() * (1 -
+		// output.getActivation()) * (correct - output.getActivation())));
+		return (output.getActivation() * (1 - output.getActivation()) * (correct - output
+				.getActivation()));
 	}
-	
-	/**Updates weights based on previously determined delta from output*/
-	public void updateWeights(){
-		double[] temp = output.getWeights(); 
+
+	public double calculateDeltaHidden(double correct) {
+
+		return (output.getActivation() * (1 - output.getActivation()) * (correct - output
+				.getActivation()));
+	}
+
+	/** Updates weights based on previously determined delta from output */
+	public void updateWeights() {
+		double[] temp = output.getWeights();
 		Neuron[] tempNeuron = output.getInputs();
-		for(int i = 0; i < output.getInputSize(); i++){
-			temp[i] += learningRate * (tempNeuron[i].getActivation()) * output.getDelta();
-			System.out.println("  weight " + tempNeuron[i].getActivation() + " " + temp[i]);
+		for (int i = 0; i < output.getInputSize(); i++) {
+			temp[i] += learningRate * (tempNeuron[i].getActivation())
+					* output.getDelta();
+			System.out.println("  weight " + tempNeuron[i].getActivation()
+					+ " " + temp[i]);
 		}
 		output.setWeights(temp);
 	}
-	
-	
-	
+
 }
--- src/SigmoidNeuronTest.java
+++ src/SigmoidNeuronTest.java
@@ -3,86 +3,85 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SigmoidNeuronTest {
-	public SigmoidNeuron neuron = new SigmoidNeuron();
-
-	@Test
-	public void testGetActivation() {
-		neuron.setActivation(5.0);
-		assertEquals(5.0, neuron.getActivation(), .001);
-		neuron.setActivation(-1.1);
-		assertEquals(-1.1, neuron.getActivation(), .001);
-	}
-	
-	@Test
-	public void testGetInputSize() {
-		InputNeuron a = new InputNeuron();
-		SigmoidNeuron b = new SigmoidNeuron();
-		Neuron[] inputArray = {a, b};
-		neuron.setInputs(inputArray);
-		assertEquals(2, neuron.getInputSize());
-	}
-	
-	@Test
-	public void testGetWeights() {
-		// TODO Initialize weights and inputs in Neuron constructor
-		InputNeuron a = new InputNeuron();
-		SigmoidNeuron b = new SigmoidNeuron();
-		Neuron[] inputArray = {a, b};
-		neuron.setInputs(inputArray);
-		double[] test = {1, 2};
-		neuron.setWeights(test);
-		double[] testTwo = neuron.getWeights();
-		for (int i = 0; i < neuron.getInputSize(); i++){
-			assertEquals(test[i], testTwo[i], .001);
-			assertNotEquals(test[i], 5, .001);
-		}
-	}
-
-	@Test
-	public void testGetInputs() {
-		InputNeuron a = new InputNeuron();
-		SigmoidNeuron b = new SigmoidNeuron();
-		a.setActivation(3);
-		b.setActivation(4);
-		Neuron[] inputArray = {a, b};
-		neuron.setInputs(inputArray);
-		Neuron[] testInputArray = neuron.getInputs();
-		for (int i = 0; i < neuron.getInputSize(); i++){
-			assertEquals(inputArray[i].getActivation(), testInputArray[i].getActivation(), .001);
-		}
-		InputNeuron c = new InputNeuron();
-		SigmoidNeuron d = new SigmoidNeuron();
-		c.setActivation(5);
-		d.setActivation(6);
-		Neuron[] inputArray2 = {a, b, c, d};
-		neuron.setInputs(inputArray2);
-		Neuron[] testInputArray2 = neuron.getInputs();
-		for (int i = 0; i < neuron.getInputSize(); i++){
-			assertNotEquals(inputArray2[i].getActivation(), 0, .001);
-			assertEquals(inputArray2[i].getActivation(), testInputArray2[i].getActivation(), .001);
-		}
-	}
+	public SigmoidNeuron neuron;
 
 	@Test
 	public void testComputeActivation() {
-		double[] weights = {1, 2};
+		double[] weights = { 1, 2 };
 		InputNeuron a = new InputNeuron();
-		SigmoidNeuron b = new SigmoidNeuron();
+		InputNeuron b = new InputNeuron();
 		a.setActivation(2);
 		b.setActivation(1);
-		Neuron[] inputArray = {a, b};
-		neuron.setInputs(inputArray);
-		neuron.setWeights(weights);
-		System.out.println("test");
+		Neuron[] inputArray = { a, b };
+		neuron = new SigmoidNeuron(inputArray, weights);
 		neuron.setActivation(neuron.computeActivation());
 		assertEquals(0.98201379003, neuron.getActivation(), .0001);
 	}
 
 	@Test
+	public void testGetActivation() {
+		InputNeuron a = new InputNeuron();
+		Neuron[] input = { a, a };
+		double[] weight = { 2, 3 };
+		neuron = new SigmoidNeuron(input, weight);
+		neuron.setActivation(5.0);
+		assertEquals(5.0, neuron.getActivation(), .001);
+		neuron.setActivation(-1.1);
+		assertEquals(-1.1, neuron.getActivation(), .001);
+	}
+
+	@Test
 	public void testGetDelta() {
+		InputNeuron a = new InputNeuron();
+		Neuron[] input = { a, a };
+		double[] weight = { 2, 3 };
+		neuron = new SigmoidNeuron(input, weight);
 		neuron.setDelta(1.2);
 		assertEquals(1.2, neuron.getDelta(), .001);
 	}
 
+	@Test
+	public void testGetInputs() {
+		InputNeuron a = new InputNeuron();
+		InputNeuron b = new InputNeuron();
+		a.setActivation(3);
+		b.setActivation(4);
+		Neuron[] inputArray = { a, b };
+		double[] weight = { 5, 4 };
+		neuron = new SigmoidNeuron(inputArray, weight);
+		Neuron[] testInputArray = neuron.getInputs();
+		for (int i = 0; i < neuron.getInputSize(); i++) {
+			assertEquals(inputArray[i].getActivation(),
+					testInputArray[i].getActivation(), .001);
+		}
+		InputNeuron c = new InputNeuron();
+		InputNeuron d = new InputNeuron();
+		c.setActivation(5);
+		d.setActivation(6);
+		Neuron[] inputArray2 = { a, b, c, d };
+		double[] weight2 = { 5, 4, 6, 3 };
+		neuron = new SigmoidNeuron(inputArray2, weight2);
+		Neuron[] testInputArray2 = neuron.getInputs();
+		for (int i = 0; i < neuron.getInputSize(); i++) {
+			assertNotEquals(inputArray2[i].getActivation(), 0, .001);
+			assertEquals(inputArray2[i].getActivation(),
+					testInputArray2[i].getActivation(), .001);
+		}
+	}
+
+	@Test
+	public void testGetWeights() {
+		// TODO Initialize weights and inputs in Neuron constructor
+		InputNeuron a = new InputNeuron();
+		InputNeuron b = new InputNeuron();
+		Neuron[] inputArray = { a, b };
+		double[] test = { 1, 2 };
+		neuron = new SigmoidNeuron(inputArray, test);
+		double[] testTwo = neuron.getWeights();
+		for (int i = 0; i < neuron.getInputSize(); i++) {
+			assertEquals(test[i], testTwo[i], .001);
+			assertNotEquals(test[i], 5, .001);
+		}
+	}
 
 }
