1a9ef69157c23d1e4a02fbbcbc8e3bc38a309e99
Peter Drake <drake@lclark.edu>
2015-05-27 12:03:16 -0700

    Reduced the number of updates in Experiment.

--- src/Experiment.java
+++ src/Experiment.java
@@ -4,7 +4,7 @@ public class Experiment {
 		Network or = new Network();	
 		double[][] training = {{0,0}, {1,0}, {0,1}, {1,1}};
 		double[] trainingCorrect = {1, 0, 1, 1};
-		int updates = 10000;
+		int updates = 1000;
 		for (int i = 0; i < updates; i++) {
 			int k = (int)(Math.random() * training.length);
 			or.train(trainingCorrect[k], training[k]);
