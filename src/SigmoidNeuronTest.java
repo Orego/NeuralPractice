b310a2fc01d1434bc6b0fddd0b97f73d50c5b185
Peter Drake <drake@lclark.edu>
2015-05-27 11:11:00 -0700

    Cleaned up SigmoidNeuronTest.

--- src/SigmoidNeuronTest.java
+++ src/SigmoidNeuronTest.java
@@ -3,17 +3,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SigmoidNeuronTest {
+
 	public SigmoidNeuron neuron;
 
 	@Test
 	public void testComputeActivation() {
-		double[] weights = { 1, 2 };
 		InputNeuron a = new InputNeuron();
 		InputNeuron b = new InputNeuron();
 		a.setActivation(2);
 		b.setActivation(1);
-		Neuron[] inputArray = { a, b };
-		neuron = new SigmoidNeuron(inputArray, weights);
+		neuron = new SigmoidNeuron(new Neuron[] {a, b}, new double[] {1, 2});
 		neuron.updateActivation();
 		assertEquals(0.98201379003, neuron.getActivation(), .0001);
 	}
@@ -21,9 +20,7 @@ public class SigmoidNeuronTest {
 	@Test
 	public void testUpdateDelta() {
 		InputNeuron a = new InputNeuron();
-		Neuron[] input = { a, a };
-		double[] weight = { 2, 3 };
-		neuron = new SigmoidNeuron(input, weight);
+		neuron = new SigmoidNeuron(new Neuron[] {a, a}, new double[] {2, 3});
 		neuron.setActivation(0.1);
 		neuron.updateDelta(1);
 		assertEquals(0.081, neuron.getDelta(), .0001);
